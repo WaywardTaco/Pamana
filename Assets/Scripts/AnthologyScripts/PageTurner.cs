17f6ac1e921ba1c8783c753a2788d0967d3c32f8
Josiah Aviso <150400184+WaywardTaco@users.noreply.github.com>
2025-03-15 21:30:55 +0800

    some little progress

---
+++ Assets/Scripts/AnthologyScripts/PageTurner.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PageTurner : MonoBehaviour 
+{
+    public void FlipPage(bool flipClose, bool isLeftPage){
+        
+    }
+}
